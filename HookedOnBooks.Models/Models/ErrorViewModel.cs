6444fd32f67f6a8bb3ac9b504aa815a6c8181702
w0771056 <w0771056@myscc.ca>
2022-03-29 07:34:37 -0400

    oops

---
+++ HookedOnBooks.Models/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HookedOnBooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
